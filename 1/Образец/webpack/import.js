293b3323b6a2a7f3c5e86430bd5fa633212620f5
Dora <yppix@yandex.ru>
2022-04-18 00:58:06 +0300

    webpack

---
+++ 1/Образец/webpack/import.js
@@ -0,0 +1,6 @@
+import Cart from '../public/js/CartComponent'
+import Error from '../public/js/ErrorComp'
+import Products from '../public/js/ProductComponent'
+import Filter from '../public/js/FilterComp'
+
+
